0e4fa1b7bc61298c80bb471bb9fcbea478cc2f89
Faseeh121 <233583@students.au.edu.pk>
2024-10-16 20:04:11 +0500

    Add files via upload

---
+++ Lab 4/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Question1
+{
+    static void Main()
+    {
+        Student st = new Student();
+        st.setname("Fasseh Anjum");
+        st.setid(233583);
+        st.setage(20);
+        st.setdep("BSCS");
+        st.display();
+    }
+}
